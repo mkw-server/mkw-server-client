dbb320deeab5f362c403b6539904a879ea46919b
Henry <henrymkwwisconsin@gmail.com>
2026-05-30 23:53:14 -0400

    Documentation: MiscPacketHandler documentation

--- payload/game/net/MiscPacketHandler.hh
+++ payload/game/net/MiscPacketHandler.hh
@@ -14,74 +14,79 @@ extern "C" {
 
 namespace Net {
 
-/*
- * MiscPacketHandler gets created when switching to RaceScene and destroyed upon
- * RaceScene exit. Has a number of misc jobs related to packets (and some not). Imports, exports,
- * and clears a number of records. Handles frame lag, timer differences, and disconnect logic.
- * Also has ties with Event and Item records
- */
+// MiscPacketHandler runs exclusively in race scene. It handles everything netcode during a race.
 class MiscPacketHandler {
 private:
     // 0x80653728
-    // Its main logic is is ran in its update(), but the business
-    // logic will only run when m_isPrepared is true. This runs in online races but
-    // also rival ghost races to update friend status
+    // The main loop. Runs when m_isPrepared is true. This runs in online races
+    // but also rival ghost races to update friend status
     void update();
 
     // 0x80654150
-    // calls bellow functions as mentioned, but manages the start of a race also by making
-    // decisions based off the timers of other players, voting to suspend/unsuspend match making
-    // calls ItemHandler::update(), EventHandler::update(), and processLagFrames()
+    // Runs only when racing. Synchronizes race start, imports, exports, and processes records
+    // specifically as a racer.
     void updateAsRacer();
 
     // 0x80654d08
-    // called by updateAsRacer(). only RH1 record gets created, both get exported to the send buffer
-    void createExportRH1ExportRaceData();
+    // Exports relevant records as a racer (as opposed to a spectator)
+    void exportAsRacer();
 
     // 0x806554a0
-    // called by updateAsRacer(). unpacks all players RH1 records. can also set m_scheduleDisconnect
+    // Unpacks and processes received RH1 records. Updates m_aidsLoadedIntoRace and time of other
+    // players. Also checks if we're out of sync of other players and disconnects us if so.
     void processRecvRH1Records();
 
     // 0x80654038
-    // called by updateAsRacer() to begin the race countdown. the countdown can start once everyone
-    // is in the race.
+    // Returns true when all players have loaded into the race
     bool isEveryoneInRace() const;
 
     // 0x80654b00
-    // tbd explanation, loops over recv RH1 records and compares other players lag frames with ours
-    // if their lag frames are greater than ours, then set GameScene[0x2539] (maybe called
-    // skipFrame?) this causes GameScene::update() to call beginFrame() and endFrame(), which I'd
-    // assume skips a frame? also takes framerate into account, so it would skip two frames at
-    // 30fps.
+    // Processes other player's lag frames. Skip frames if someone has skipped more frames than us.
+    // Skips one frame if framerate is 60 fps and two if 30 fps.
     void processLagFrames();
 
     // 0x806544a8
-    // returns 0 if playerId isn't in the room
-    // returns aidTimeSinceRaceStart if in a race (?) and difference between
-    // m_elapsedTimeSinceRaceStart and aid's time is less than 600 ms (not a bunch of)
-    // returns m_elapsedTimeSinceRaceStart otherwise
-    u32 getRH1Timer(s32 playerId) const;
+    // Gets the passed in player's time since countdown if its not terribly out of sync
+    u32 getPlayerTimeSinceCountdown(s32 playerId) const;
 
+    // Runs update() when true
     bool m_isPrepared;
-    bool m_scheduleDisconnect; // triggers a dc when set
+
+    // Schecules a disconnect. Set if we're out of sync with other players.
+    bool m_scheduleDisconnect;
+
+    // Padding
     u8 _002[0x004 - 0x002];
-    u32 m_aidsWithRH1Seed;         // bitfield by aid, 1 when aid's seed is set
-    u32 m_aidLastSentRoomOrSelect; // aid bitfield, 1 << aid for select, 0 for room
 
-    // bitfield by aid, used to (partially) decide to dc/stop a player, but the conditions that get
-    // it set are unclear because it gets set in updateBitfields() when RH1Record[0x17] != 0, but
-    // that field is also unknown, making this one unknown.
-    u32 m_shouldStopAid;
-    s16 m_myLagFrames;   // refers to frames that didnt get rendered due to lag, gets updated in
-                         // processLagFrames()
-    s16 m_countdownTime; // set to 3000 ms,
-    RaceDataRecord m_sendRaceDataRecords[2]; // 0x14 - 0x94
-    EventRecord m_sendEventRecords;          // 0x94 - 0x18c
+    // Bitfield set by RH1.raceSeed
+    u32 m_aidsLoadedIntoRace;
+
+    // Bitfield set if a room or select record was sent
+    u32 m_aidLastSentRoomOrSelect;
+
+    // Bitfield that stops non-racers (the dc animation?)
+    u32 m_aidShouldStop;
+
+    // Number of frames skipped
+    s16 m_localLagFrames;
+
+    // Timer used to syncronize the countdown start with other players, adjusted on the fly.
+    // Countdown starts when this is negative.
+    s16 m_introCameraCountdown;
+
+    // Outgoing RaceData records. One per local player
+    RaceDataRecord m_sendRaceDataRecords[2];
+
+    // Outgoing Event record.
+    EventRecord m_sendEventRecords;
+
+    // Padding
     u8 _18c[0x190 - 0x18c];
-    // last time we updated our friends statuses. updated about once every 10 seconds
-    OSTime m_elapsedTimeSinceFriendStatusUpdated;
 
-    // Set to the countdown time, but only after the countdown ends
+    // Time since friend status was last retrieved
+    OSTime m_TimeSinceFriendStatusUpdated;
+
+    // Set to the countdown time, but set after the countdown ends
     u32 m_aidsTimeSinceCountdown[MAX_PLAYER_COUNT];
 
     static MiscPacketHandler *s_instance;
