3007c1c8a32d427f4a2c9b062b85aab6fc172b98
Henry <henry@hwcari.dev>
2026-07-26 15:25:26 -0400

    Net::RH2 --> Net::RaceMode

--- payload/game/net/RacePacketHolder.hh
+++ payload/game/net/RacePacketHolder.hh
@@ -7,8 +7,8 @@
 #include "game/net/records/Header.hh"
 #include "game/net/records/Item.hh"
 #include "game/net/records/RH1.hh"
-#include "game/net/records/RH2.hh"
 #include "game/net/records/RaceData.hh"
+#include "game/net/records/RaceMode.hh"
 #include "game/net/records/User.hh"
 
 namespace Net {
@@ -16,7 +16,7 @@ namespace Net {
 enum class RecordType : u8 {
     Header = 0,
     RaceHeader1 = 1,
-    RaceHeader2 = 2,
+    RaceMode = 2,
     RoomSelect = 3, // Room is used when the friend room is open, select otherwise
     RaceData = 4,
     User = 5,
@@ -33,8 +33,8 @@ public:
             return reinterpret_cast<RecordHolder<T> *>(m_header);
         case RecordType::RaceHeader1:
             return reinterpret_cast<RecordHolder<T> *>(m_raceHeader1);
-        case RecordType::RaceHeader2:
-            return reinterpret_cast<RecordHolder<T> *>(m_raceHeader2);
+        case RecordType::RaceMode:
+            return reinterpret_cast<RecordHolder<T> *>(m_raceMode);
         case RecordType::RoomSelect:
             return reinterpret_cast<RecordHolder<T> *>(m_roomSelect);
         case RecordType::RaceData:
@@ -58,8 +58,8 @@ public:
         return m_raceHeader1;
     }
 
-    RecordHolder<RH2Record> *rh2() {
-        return m_raceHeader2;
+    RecordHolder<RaceModeRecord> *raceMode() {
+        return m_raceMode;
     }
 
     RecordHolder<void> *roomSelect() {
@@ -98,7 +98,7 @@ public:
 private:
     RecordHolder<Header> *m_header;
     RecordHolder<RH1Record> *m_raceHeader1;
-    RecordHolder<RH2Record> *m_raceHeader2;
+    RecordHolder<RaceModeRecord> *m_raceMode;
     // TODO: Room is present while in a room that hasn't started and select is for voting
     // so whats the best way to represent this?
     RecordHolder<void> *m_roomSelect;
